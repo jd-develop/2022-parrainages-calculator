f94afcb15cb3deace91c323a7392837f5b8b877f
Jean Dubois <jd-dev@laposte.net>
2022-02-07 13:29:59 +0100

    it works but can be better

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Python script that calculates number of 'parrainages' for all the candidates of the French 2022 Présidence de la république's elections.
 
 The data is auto-downloaded from the [Conseil Constitutionel's website](https://presidentielle2022.conseil-constitutionnel.fr/les-parrainages/tous-les-parrainages-valides.html). You can download it as a file in the 'Téléchargements' (downloads) section.
+
+# Python
+Requires Python 3.9 or Python 3.10. If you have a lower version, just delete the `: list[dict]` in make_graph.py.
--- main.py
+++ main.py
@@ -3,7 +3,35 @@
 import json
 import pprint
 import urllib.request
+import make_graph
 from datetime import datetime
+
+
+def list_candidates_from_request(response_: dict):
+    candidates_ = {}
+    for parrain_ in response_:
+        candidate_ = parrain_['Candidat']
+        try:
+            candidates_[candidate_] += 1
+        except KeyError:
+            candidates_[candidate_] = 1
+    return candidates_
+
+
+def list_departments_from_request(response_: dict):
+    departments_ = {}
+    for parrain in response_:
+        department = parrain['Departement']
+        candidate = parrain['Candidat']
+        try:
+            departments_[department][candidate] += 1
+        except KeyError:
+            try:
+                departments_[department][candidate] = 1
+            except KeyError:
+                departments_[department] = {candidate: 1}
+    return departments_
+
 
 url_of_parrainages = "https://presidentielle2022.conseil-constitutionnel.fr/telechargement/parrainagestotal.json"
 actual_file_name = f"json_history/{datetime.today().strftime('%Y-%m-%d')}.json"
@@ -17,28 +45,31 @@ with urllib.request.urlopen(url_of_parrainages) as response:  # response is HTTP
         response_dict = json.load(file)
         file.close()
 
-candidates = {}
-for parrain in response_dict:
-    candidate = parrain['Candidat']
-    try:
-        candidates[candidate] += 1
-    except KeyError:
-        candidates[candidate] = 1
+candidates = list_candidates_from_request(response_dict)
 
-departments = {}
-for parrain in response_dict:
-    department = parrain['Departement']
-    candidate = parrain['Candidat']
-    try:
-        departments[department][candidate] += 1
-    except KeyError:
-        try:
-            departments[department][candidate] = 1
-        except KeyError:
-            departments[department] = {candidate: 1}
+departments = list_departments_from_request(response_dict)
 
-print("TODAY PARRAINAGES:")
+most_by_department = {}
+for department_ in departments:
+    most_by_department[department_] = max(departments[department_], key=departments[department_].get)
+
+print("ACTUAL PARRAINAGES:")
 pprint.pprint(candidates)
 
-print("BY DEPARTMENT:")
+print("\nBY DEPARTMENT:")
 pprint.pprint(departments)
+
+print("\nMOST POPULAR CANDIDATES BY DEPARTMENT:")
+pprint.pprint(most_by_department)
+
+# graph
+list_of_all_candidates_dicts = []
+for file in make_graph.os.listdir("json_history"):  # os is imported in make_graph
+    if file.endswith(".json") and file.startswith("2022-"):
+        with open(f"json_history/{file}", "r", encoding="utf-8-sig") as file_:
+            response_dict = json.load(file_)
+            file_.close()
+        list_of_all_candidates_dicts.append(list_candidates_from_request(response_dict))
+
+
+make_graph.display_graph(list_of_all_candidates_dicts)
